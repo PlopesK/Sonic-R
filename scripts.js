b8cdb8987b8055b4aace50bf61abce917ce36a38
PlopesK <popi081004@hotmail.com>
2023-03-09 18:07:55 -0300

    Game functioning, but need more details

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const sonic = document.getElementById("sonic");
+const obstacle = document.getElementById("obstacle");
 function jump() {
     if (sonic.classList != "jump") {
         sonic.classList.add("jump");
@@ -8,6 +9,19 @@ function jump() {
         }, 400);
     }   
 }
+
+let isAlive = setInterval(function () {
+    let sonicTop = parseInt(
+        window.getComputedStyle(sonic).getPropertyValue("top"));
+
+    let obstLeft = parseInt(
+        window.getComputedStyle(obstacle).getPropertyValue("left"));
+
+    if (obstLeft < 40 && obstLeft > 0 && sonicTop >= 140) {
+        alert("Game Over!");
+        location.reload()
+    }
+}, 10);
 
 document.addEventListener("keydown", function (event) {
     jump();
