6b3f657776d3873e36f4ec4b79dea5a4e397ffcb
PlopesK <popi081004@hotmail.com>
2023-03-10 15:31:38 -0300

    Added a play buitton

--- scripts.js
+++ scripts.js
@@ -1,28 +1,35 @@
-const sonic = document.getElementById("sonic");
-const obstacle = document.getElementById("obstacle");
-function jump() {
-    if (sonic.classList != "jump") {
-        sonic.classList.add("jump");
-        
-        setTimeout(function () {
-            sonic.classList.remove("jump");
-        }, 400);
-    }   
-}
+/* Game */
+function game() {
+    const sonic = document.getElementById("sonic");
+    const obstacle = document.getElementById("obstacle");
+    const button = document.getElementById("play");
+    button.classList.add("click");
+    obstacle.classList.add("block");
 
-let isAlive = setInterval(function () {
-    let sonicTop = parseInt(
-        window.getComputedStyle(sonic).getPropertyValue("top"));
-
-    let obstLeft = parseInt(
-        window.getComputedStyle(obstacle).getPropertyValue("left"));
-
-    if (obstLeft < 40 && obstLeft > 0 && sonicTop >= 140) {
-        alert("Game Over!");
-        location.reload()
+    function jump() {
+        if (sonic.classList != "jump") {
+            sonic.classList.add("jump");
+            
+            setTimeout(function () {
+                sonic.classList.remove("jump");
+            }, 400);
+        }   
     }
-}, 10);
 
-document.addEventListener("keydown", function (event) {
-    jump();
-});
+    let isAlive = setInterval(function () {
+        let sonicTop = parseInt(
+            window.getComputedStyle(sonic).getPropertyValue("top"));
+
+        let obstLeft = parseInt(
+            window.getComputedStyle(obstacle).getPropertyValue("left"));
+
+        if (obstLeft < 40 && obstLeft > 0 && sonicTop >= 140) {
+            alert("Game Over!");
+            location.reload()
+        }
+    }, 10);
+
+    document.addEventListener("keydown", function (event) {
+        jump();
+    });
+}
