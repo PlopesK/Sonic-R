c899b5b00586d8a682dbbfbe9e006d4b5581594a
PlopesK <popi081004@hotmail.com>
2023-03-14 19:14:49 -0300

    Changed a lil bit the code

--- src/obstacle.js
+++ src/obstacle.js
@@ -1,11 +1,7 @@
 function enemy(){ 
   const obstacle1 = document.getElementById("badnik1");
   const obstacle2 = document.getElementById("badnik2");
-  badnik1();
-
-  function badnik1(){
-    obstacle1.classList.add("block");
-    obstacle1.style.backgroundImage = "url(sprites/badnik1.gif)";
+  firstanim();
 
     obstacle1.addEventListener('animationend', () => {
       setTimeout(function () {
@@ -14,7 +10,7 @@ function enemy(){
         const maxDuration1 = 2500;
         const randomDuration1 = Math.floor(Math.random() * (maxDuration1 - minDuration1 + 1)) + minDuration1;
         const animationDuration1 = `${randomDuration1 / 1000}s`;
-  
+      
         obstacle1.style.animationDuration = animationDuration1;
         obstacle1.classList.remove('block');
         void obstacle1.offsetWidth;
@@ -22,15 +18,13 @@ function enemy(){
       }, 3000);
       obstacle1.style.backgroundImage = "";
     });
-  }
 
-  function badnik2() {
     obstacle2.addEventListener('animationend', () => {
       setTimeout(function () {
         obstacle2.classList.add("block");
         obstacle2.style.backgroundImage = "url(sprites/badnik2.gif)";
         obstacle2.style.top = '20px';
-        const minDuration2 = 3000;
+        const minDuration2 = 1000;
         const maxDuration2 = 5000;
         const randomDuration2 = Math.floor(Math.random() * (maxDuration2 - minDuration2 + 1)) + minDuration2;
         const animationDuration2 = `${randomDuration2 / 1000}s`;
@@ -39,16 +33,17 @@ function enemy(){
         obstacle2.classList.remove('block');
         void obstacle2.offsetWidth;
         obstacle2.classList.add('block');
-      }, 11000);
+      }, 9000);
       obstacle2.style.backgroundImage = "";
     });
+
+  function firstanim() {
+    obstacle1.classList.add("block");
+    obstacle1.style.backgroundImage = "url(sprites/badnik1.gif)";
+    setTimeout(function() {
+      obstacle2.classList.add("block");
+      obstacle2.style.top = '-100px';
+      return;
+    }, 2000)
   }
-
-  let start = setTimeout(function() {
-    obstacle2.classList.add("block");
-    obstacle2.style.top = '-100px';
-    badnik2();
-
-    clearTimeout(start);
-  }, 2000);
 }
