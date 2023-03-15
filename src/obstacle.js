356221586f6218bc840340ba35e80fe023b84f7a
PlopesK <popi081004@hotmail.com>
2023-03-14 22:37:50 -0300

    Changed enemies code since it was flickering a lot

--- src/obstacle.js
+++ src/obstacle.js
@@ -1,46 +1,50 @@
 /* 👾👾👾👾Enemy👾👾👾👾 */
-function enemy(){ 
+function enemy() {
   const obstacle1 = document.getElementById("badnik1");
   const obstacle2 = document.getElementById("badnik2");
-  badnik1();
-  badnik2();
+  const background = document.getElementById("background");
 
-  function badnik1() {
-    setTimeout(function () {
-      obstacle1.classList.add("block");
-      obstacle1.style.backgroundImage = "url(sprites/badnik1.gif)";
-      const minDuration1 = 1500;
-      const maxDuration1 = 2500;
-      const randomDuration1 = Math.floor(Math.random() * (maxDuration1 - minDuration1 + 1)) + minDuration1;
-      const animationDuration1 = `${randomDuration1 / 1000}s`;
-        
-      obstacle1.style.animationDuration = animationDuration1;
-      obstacle1.classList.remove('block');
-      void obstacle1.offsetWidth;
-      obstacle1.classList.add('block');
-    }, 3000);
-    obstacle1.addEventListener("animationend", function() {
-      obstacle1.style.backgroundImage = "";
-    })
+  function moveObstacle1() {
+    obstacle1.style.backgroundImage = "url(sprites/badnik1.gif)";
+    obstacle1.style.transform = "translateX(100%)";
+    const duration1 = 3000;
+    const distance1 = obstacle1.offsetWidth + background.offsetWidth;
+    const speed1 = distance1 / duration1;
+    const startTime1 = performance.now();
+    requestAnimationFrame(function animateObstacle1(time) {
+      const elapsedTime1 = time - startTime1;
+      const distanceCovered1 = elapsedTime1 * speed1; 
+      obstacle1.style.transform = `translateX(calc(100% - ${distanceCovered1}px))`;
+      if (distanceCovered1 < distance1) {
+        requestAnimationFrame(animateObstacle1);
+      } else {
+        obstacle1.style.backgroundImage = "";
+        setTimeout(moveObstacle1, Math.floor(Math.random() * 2000) + 4000);
+      }
+    });
   }
 
-  function badnik2() {
-    setTimeout(function () {
-      obstacle2.classList.add("block");
-      obstacle2.style.backgroundImage = "url(sprites/badnik2.gif)";
-      obstacle2.style.top = '20px';
-      const minDuration2 = 1000;
-      const maxDuration2 = 2000;
-      const randomDuration2 = Math.floor(Math.random() * (maxDuration2 - minDuration2 + 1)) + minDuration2;
-      const animationDuration2 = `${randomDuration2 / 1000}s`;
-    
-      obstacle2.style.animationDuration = animationDuration2;
-      obstacle2.classList.remove('block');
-      void obstacle2.offsetWidth;
-      obstacle2.classList.add('block');
-    }, 12000);
-    obstacle2.addEventListener("animationend", function() {
-      obstacle2.style.backgroundImage = "";
-    })
+  function moveObstacle2() {
+    obstacle2.style.backgroundImage = "url(sprites/badnik2.gif)";
+    obstacle2.style.transform = "translateX(100%)";
+    obstacle2.style.top = "20px";
+    const duration2 = 3000;
+    const distance2 = obstacle2.offsetWidth + background.offsetWidth;
+    const speed2 = distance2 / duration2;
+    const startTime2 = performance.now(); 
+    requestAnimationFrame(function animateObstacle2(time) {
+      const elapsedTime2 = time - startTime2;
+      const distanceCovered2 = elapsedTime2 * speed2;
+      obstacle2.style.transform = `translateX(calc(100% - ${distanceCovered2}px))`;
+      if (distanceCovered2 < distance2) {
+        requestAnimationFrame(animateObstacle2); 
+      } else {
+        obstacle2.style.backgroundImage = "";
+        setTimeout(moveObstacle2, Math.floor(Math.random() * 5000) + 6000);
+      }
+    });
   }
+
+  setTimeout(moveObstacle1, Math.floor(Math.random() * 2000) + 4000);
+  setTimeout(moveObstacle2, Math.floor(Math.random() * 5000) + 6000)
 }
