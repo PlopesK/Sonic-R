c22ff4b73dd8feadcad4e2748a0c8a04960fbe39
PlopesK <popi081004@hotmail.com>
2023-03-29 19:40:30 -0300

    Highscore system (alpha)

--- src/score.js
+++ src/score.js
@@ -1,6 +1,7 @@
 /* 🎰🎰🎰🎰Score🎰🎰🎰🎰 */
 let millisecond = 0;
 let points = 0;
+let highScore = 0;
 let cron;
 
 function crono(){
@@ -32,10 +33,17 @@ function score(){
         HigherScoreHit.addEventListener("ended", function() {
             score.classList.remove("scoreAnimation");
         });
+    } if (points > highScore) {
+        highScore = points;
+        localStorage.setItem('highScore', highScore);
     }
-    score.innerText = returnData(points);
+    score.innerText = returnData(points) + ' | High Score: ' + returnData(highScore);
 }
 
 function returnData(input) {
     return input >= 10 ? input : `0${input}`
+}
+
+window.onload = function() {
+    highScore = localStorage.getItem('highScore') || 0;
 }
