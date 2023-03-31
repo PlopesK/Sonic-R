9b03c9c4c5bdf33c6e9590a44bffe6cc6b237fc0
PlopesK <popi081004@hotmail.com>
2023-03-30 22:34:46 -0300

    Started to add Metal Sonic + adjusts

---
+++ src/characters.js
@@ -0,0 +1,63 @@
+window.addEventListener("DOMContentLoaded", function (event) {
+    /* Characters */
+    const selectedCharacter = localStorage.getItem('characters');
+    if(selectedCharacter === 'Sonic' || selectedCharacter == null) {
+        Sonic();
+    } if (selectedCharacter === 'ClassicSonic') {
+        ClassicSonic();
+    } else {
+        MetalSonic();
+    }
+
+    /* 🖼️🖼️🖼️🖼️Pre-loading images🖼️🖼️🖼️🖼️ */
+    var charURL = ['sprites/sonic-idle.gif', "sprites/sonic-start.gif", "sprites/sonic-run.gif", 
+    "sprites/sonic-jump.gif", 'sprites/sonic-damage.gif', 'sprites/sonic-death.gif', 'sprites/life-icon,.gif',
+    "sprites/ClassicSonic/sonic3-idle.gif", 'sprites/ClassicSonic/sonic3-start.gif', 'sprites/ClassicSonic/sonic3-run.gif',
+    'sprites/ClassicSonic/sonic3-jump.gif', 'sprites/ClassicSonic/sonic3-doublejump.gif', 'sprites/ClassicSonic/sonic3-damage.gif',
+    'sprites/ClassicSonic/sonic3-death.gif'];
+    var images = [];
+    for (var i = 0; i < charURL.length; i++) {
+        var img = new Image();
+        img.src = charURL[i];
+        images.push(img);
+    }
+});
+
+function ClassicSonic() {
+    localStorage.setItem('characters', 'ClassicSonic');
+
+    document.documentElement.style.setProperty('--Idle', 'url(sprites/ClassicSonic/sonic3-idle.gif)');
+    document.documentElement.style.setProperty('--Start', 'url(sprites/ClassicSonic/sonic3-start.gif)');
+    document.documentElement.style.setProperty('--Run', 'url(sprites/ClassicSonic/sonic3-run.gif)');
+    document.documentElement.style.setProperty('--Jump', 'url(sprites/ClassicSonic/sonic3-jump.gif)');
+    document.documentElement.style.setProperty('--DoubleJump', 'url(sprites/ClassicSonic/sonic3-doublejump.gif)');
+    document.documentElement.style.setProperty('--Damage', 'url(sprites/ClassicSonic/sonic3-damage.gif)');
+    document.documentElement.style.setProperty('--Death', 'url(sprites/ClassicSonic/sonic3-death.gif)');
+    document.getElementById("life-icon").src="sprites/ClassicSonic/sonic3-life.gif";
+}
+
+function Sonic() {
+    localStorage.setItem('characters', 'Sonic');
+
+    document.documentElement.style.setProperty('--Idle', 'url(sprites/sonic-idle.gif)');
+    document.documentElement.style.setProperty('--Start', 'url(sprites/sonic-start.gif)');
+    document.documentElement.style.setProperty('--Run', 'url(sprites/sonic-run.gif)');
+    document.documentElement.style.setProperty('--Jump', 'url(sprites/sonic-jump.gif)');
+    document.documentElement.style.setProperty('--DoubleJump', 'url(sprites/sonic-doublejump.gif)');
+    document.documentElement.style.setProperty('--Damage', 'url(sprites/sonic-damage.gif)');
+    document.documentElement.style.setProperty('--Death', 'url(sprites/sonic-death.gif)');
+    document.getElementById("life-icon").src="sprites/life-icon.gif";
+}
+
+function MetalSonic() {
+    localStorage.setItem('characters', 'MetalSonic');
+
+    document.documentElement.style.setProperty('--Idle', 'url(sprites/MetalSonic/metalsonic-idle.gif)');
+    document.documentElement.style.setProperty('--Start', 'url(sprites/MetalSonic/metalsonic-start.gif)');
+    document.documentElement.style.setProperty('--Run', 'url(sprites/MetalSonic/metalsonic-run.gif)');
+    document.documentElement.style.setProperty('--Jump', 'url(sprites/ClassicSonic/sonic3-jump.gif)');
+    document.documentElement.style.setProperty('--DoubleJump', 'url(sprites/ClassicSonic/sonic3-doublejump.gif)');
+    document.documentElement.style.setProperty('--Damage', 'url(sprites/ClassicSonic/sonic3-damage.gif)');
+    document.documentElement.style.setProperty('--Death', 'url(sprites/ClassicSonic/sonic3-death.gif)');
+    document.getElementById("life-icon").src="sprites/ClassicSonic/sonic3-life.gif";
+}
--- src/loading.js
+++ src/loading.js
@@ -28,23 +28,13 @@ window.addEventListener("DOMContentLoaded", function (event) {
         blueTheme();
       }
 
-      /* Characters */
-    const selectedCharacter = localStorage.getItem('characters');
-    if(selectedCharacter === 'Sonic' || selectedCharacter == null) {
-        Sonic();
-      } else {
-        ClassicSonic();
-      }
-
     /* 🖼️🖼️🖼️🖼️Pre-loading images🖼️🖼️🖼️🖼️ */
-    var imageUrls = ['sprites/sonic-idle.gif', "sprites/sonic-start.gif", "sprites/sonic-run.gif", 
-    "sprites/sonic-jump.gif", 'sprites/sonic-damage.gif', 'sprites/sonic-death.gif', 'sprites/Object1.png', 'sprites/Object2.png', 
-    'sprites/Object3.gif', 'sprites/Object4.gif', 'sprites/Object5.gif', 'sprites/Object6.png', 
-    'sprites/Object7.gif', 'sprites/Object8.gif', 'sprites/badnik1.gif', 'sprites/badnik2.gif', "sprites/ground.png", 
-    "sprites/background.gif", 'images/sonic.ico', 'images/Sonic_Runners_logo.png', "sprites/score.png", 
-    "images/footer-bg.gif", "images/header.png", 'sprites/life-icon.gif', 'sprites/0.png', 'sprites/1.png', 
-    'sprites/2.png', 'sprites/3.png', 'images/sega-dark.png', 'images/footer-bg-dark.gif', 'images/header-dark.jpg', 
-    "sprites/gameover.png", "sprites/gameover-sonic.gif"];
+    var imageUrls = ['sprites/Object1.png', 'sprites/Object2.png', 'sprites/Object3.gif', 'sprites/Object4.gif', 
+    'sprites/Object5.gif', 'sprites/Object6.png', 'sprites/Object7.gif', 'sprites/Object8.gif', 'sprites/badnik1.gif', 
+    'sprites/badnik2.gif', "sprites/ground.png", "sprites/background.gif", 'images/sonic.ico', 
+    'images/Sonic_Runners_logo.png', "sprites/score.png", "images/footer-bg.gif", "images/header.png", 
+    'sprites/0.png', 'sprites/1.png', 'sprites/2.png', 'sprites/3.png', 'images/sega-dark.png', 
+    'images/footer-bg-dark.gif', 'images/header-dark.jpg', "sprites/gameover.png", "sprites/gameover-sonic.gif"];
     var images = [];
     for (var i = 0; i < imageUrls.length; i++) {
         var img = new Image();
